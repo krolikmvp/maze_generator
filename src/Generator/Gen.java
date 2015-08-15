907b43cf3b4f81c276e9ba977c8a8d50fa7419a7
Damian <saia6@wp.pl>
2015-08-15 23:56:49 +0200

    Maze generator done. Max heap size problem

--- src/Generator/Gen.java
+++ src/Generator/Gen.java
@@ -8,22 +8,22 @@ import java.util.Random;
 
 class topMask {
 	public int id = 0;
-	public static int[][] mask = { { 2, 2, 2 }, { 0, 0, 0 }, { 0, 0, 0 } };
+	public static int[][] mask = { { 2, 2, 2 }, { 2, 2, 2 }, { 0, 0, 0 } };
 }
 
 class bottomMask {
 	public int id = 1;
-	public static int[][] mask = { { 0, 0, 0 }, { 0, 0, 0 }, { 2, 2, 2 } };
+	public static int[][] mask = { { 0, 0, 0 }, { 2, 2, 2 }, { 2, 2, 2 } };
 }
 
 class leftMask {
 	public int id = 2;
-	public static int[][] mask = { { 2, 0, 0 }, { 2, 0, 0 }, { 2, 0, 0 } };
+	public static int[][] mask = { { 2, 2, 0 }, { 2, 2, 0 }, { 2, 2, 0 } };
 }
 
 class rightMask {
 	public int id = 3;
-	public static int[][] mask = { { 0, 0, 2 }, { 0, 0, 2 }, { 0, 0, 2 } };
+	public static int[][] mask = { { 0, 2, 2 }, { 0, 2, 2 }, { 0, 2, 2 } };
 }
 
 public class Gen {
@@ -38,9 +38,7 @@ public class Gen {
 		this.sizey = y;
 		generator = new Random();
 		this.maze = new int[x][y];
-		/*
-		 * for (int[] row: maze) Arrays.fill(row, 0);
-		 */
+
 		for (int i = 0; i < maze.length; i++) {
 			for (int j = 0; j < maze[i].length; j++) {
 				if (i == 0 || i == y - 1 || j == 0 || j == x - 1) {
@@ -75,10 +73,10 @@ public class Gen {
 
 		int spos[] = { 5, 5 };
 		flag = false;
-		maze[0][0] = 1;
+		maze[5][5] = 1;
 		while (flag == false) {
 
-			if (checkNeighbour(spos) == 0)
+			if (checkNeighbour(3, spos) == 0 || checkNeighbour(3, spos) == 1)
 				break;
 		}
 
@@ -95,87 +93,122 @@ public class Gen {
 		}
 	}
 
-	public int putMask(int[][] mask, int[] pos) {
+	public int putMask(int[][] mask, int[] pos, int dir) {
 		int sum = 0;
+		// /top
+		switch (dir) {
+		case 0:
+			sum += maze[pos[0] - 1][pos[1] - 2] * mask[0][0];
+			sum += maze[pos[0] - 1][pos[1] - 1] * mask[1][0];
+			sum += maze[pos[0]][pos[1] - 2] * mask[0][1];
+			sum += maze[pos[0]][pos[1] - 1] * mask[1][1];
+			sum += maze[pos[0] + 1][pos[1] - 2] * mask[0][2];
+			sum += maze[pos[0] + 1][pos[1] - 1] * mask[1][2];
+			break;
+		// bottom
+		case 1:
+			sum += maze[pos[0] - 1][pos[1] + 2] * mask[1][0];
+			sum += maze[pos[0] - 1][pos[1] + 1] * mask[2][0];
+			sum += maze[pos[0]][pos[1] + 2] * mask[1][1];
+			sum += maze[pos[0]][pos[1] + 1] * mask[2][1];
+			sum += maze[pos[0] + 1][pos[1] + 2] * mask[1][2];
+			sum += maze[pos[0] + 1][pos[1] + 1] * mask[2][2];
+			break;
+		// left
+		case 2:
+			sum += maze[pos[0] - 1][pos[1] - 1] * mask[0][1];
+			sum += maze[pos[0] - 2][pos[1] - 1] * mask[0][0];
+			sum += maze[pos[0] - 1][pos[1]] * mask[1][1];
+			sum += maze[pos[0] - 2][pos[1]] * mask[1][0];
+			sum += maze[pos[0] - 1][pos[1] + 1] * mask[2][1];
+			sum += maze[pos[0] - 2][pos[1] + 1] * mask[2][0];
+			break;
 
-		sum+=  maze[pos[0] - 1][pos[1] - 2] * mask[0][0];
-		sum+=  maze[pos[0]][pos[1] - 2] * mask[0][1];
-		sum+=  maze[pos[0] + 1][pos[1] - 2] * mask[0][2];
-		
-		sum+=  maze[pos[0] - 2][pos[1]-1] * mask[0][0];
-		sum+=  maze[pos[0]-2][pos[1]] * mask[1][0];
-		sum+=  maze[pos[0]-2][pos[1]+1] * mask[2][0];
-		
-		sum+=  maze[pos[0]+2][pos[1]-1] * mask[0][2];
-		sum+=  maze[pos[0]+2][pos[1]] * mask[1][2];
-		sum+=  maze[pos[0]+2][pos[1]+1] * mask[2][2];
-		
-		sum+=  maze[pos[0] - 1][pos[1] + 2] * mask[2][0];
-		sum+=  maze[pos[0]][pos[1] + 2] * mask[2][1];
-		sum+=  maze[pos[0] + 1][pos[1] + 2] * mask[2][2];
-		
+		// right
+		case 3:
+			sum += maze[pos[0] + 1][pos[1] - 1] * mask[0][1];
+			sum += maze[pos[0] + 2][pos[1] - 1] * mask[0][2];
+			sum += maze[pos[0] + 1][pos[1]] * mask[1][1];
+			sum += maze[pos[0] + 2][pos[1]] * mask[1][2];
+			sum += maze[pos[0] + 1][pos[1] + 1] * mask[2][1];
+			sum += maze[pos[0] + 2][pos[1] + 1] * mask[2][2];
+			break;
+		}
 		return sum;
 	}
 
-	public int checkNeighbour(int[] pos) {
+	public int checkNeighbour(int dir, int[] pos) {
+		int in = 0;
+		int result = 1;
+		int[] newpos = new int[2];
+		switch (dir) {
+		case 0:
 
-		int[] array = { 0, 1, 2, 3 };
-		ShuffleArray(array);
-		int result=1;
+			if ((result = putMask(topMask.mask, pos, dir)) < 1) {
+				System.out.println("result :" + result + " oldposx " + pos[0]
+						+ "   oldposy" + pos[1]);
+				newpos[0] = pos[0];
+				newpos[1] = pos[1] - 1;
+				System.out.println("result :" + result + " posx " + newpos[0]
+						+ "   posy" + newpos[1]);
+			}
+			break;
+		case 1:
+			if ((result = putMask(bottomMask.mask, pos, dir)) < 1) {
+				System.out.println("result :" + result + " oldposx " + pos[0]
+						+ "   oldposy" + pos[1]);
+				newpos[0] = pos[0];
+				newpos[1] = pos[1] + 1;
+				System.out.println("result :" + result + " posx " + newpos[0]
+						+ "   posy" + newpos[1]);
+			}
+			break;
+		case 2:
+			if ((result = putMask(leftMask.mask, pos, dir)) < 1) {
+				System.out.println("result :" + result + " oldposx " + pos[0]
+						+ "   oldposy" + pos[1]);
+				newpos[0] = pos[0] - 1;
+				newpos[1] = pos[1];
+				System.out.println("result :" + result + " posx " + newpos[0]
+						+ "   posy" + newpos[1]);
+			}
+			break;
+		case 3:
+			if ((result = putMask(rightMask.mask, pos, dir)) < 1) {
+				System.out.println("result :" + result + " oldposx " + pos[0]
+						+ "   oldposy" + pos[1]);
+				newpos[0] = pos[0] + 1;
+				newpos[1] = pos[1];
+				System.out.println("result :" + result + " posx " + newpos[0]
+						+ "   posy" + newpos[1]);
+			}
+			break;
+		}
 
-		for (int i = 0; i < 4; ++i) {
-			System.out.println("posx posy :" + pos[0] + "  " + pos[1]);
-			int choice = array[i];
-			int[] newpos = new int[2];
-			switch (choice) {
-
-			case 0:
-
-				if ((result = putMask(topMask.mask, pos)) < 1) {
-					System.out.println("result :" + result);
-					newpos[0] = pos[0];
-					newpos[1] = pos[1] - 1;
-				}
-				break;
-			case 1:
-				if ((result = putMask(bottomMask.mask, pos)) < 1) {
-					System.out.println("result :" + result);
-					newpos[0] = pos[0];
-					newpos[1] = pos[1] + 1;
-				}
-				break;
-			case 2:
-				if ((result = putMask(leftMask.mask, pos)) < 1) {
-					System.out.println("result :" + result);
-					newpos[0] = pos[0] - 1;
-					newpos[1] = pos[1];
-				}
-				break;
-			case 3:
-				if ((result = putMask(rightMask.mask, pos)) < 1) {
-					System.out.println("result :" + result);
-					newpos[0] = pos[0] + 1;
-					newpos[1] = pos[1];
-				}
-				break;
+		if (newpos[0] < 0 || newpos[1] < 0 || newpos[1] > sizey
+				|| newpos[0] > sizex) {
+			return 0;
+		}
+		if (result > 0) {
+			if (in == 0) {
+				return 0;
+			} else {
+				return 1;
 			}
 
-			if (newpos[0] < 0 || newpos[1] < 0 || newpos[1] > sizey
-					|| newpos[0] > sizex) {
-				continue;
-			}
-			if (result < 1) {
-				maze[newpos[0]][newpos[1]] = 1;
-				checkNeighbour(newpos);
+		} else {
+			in++;
+			maze[newpos[0]][newpos[1]] = 1;
+			int[] array = { 0, 1, 2, 3 };
+			ShuffleArray(array);
+			for (int i = 0; i < 4; ++i) {
+
+				checkNeighbour(array[i], newpos);
+
 			}
 		}
-		return 0;
-	}
 
-	/*
-	 * public static void main(String args[]) { System.out.println("START"); Gen
-	 * maze = new Gen(10, 10); maze.genMaze();
-	 * System.out.println("asd"+maze.maze[0][0]); }
-	 */
+		return 1;
+	}
 
 }
